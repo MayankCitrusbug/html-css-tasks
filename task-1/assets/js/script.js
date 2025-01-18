09313a3d7cdb251c388f5a523a11fb899880c5e8
MayankCitrusbug <mayank.citrusbug@gmail.com>
2025-01-18 19:35:51 +0530

    working on footer

--- task-1/assets/js/script.js
+++ task-1/assets/js/script.js
@@ -3,7 +3,7 @@ showSlides();
 
 function showSlides() {
     let i;
-    let slides = document.getElementsByClassName("uke__right__carousel_container__slides");
+    let slides = document.getElementsByClassName("uke__carousel__slides");
     let dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
@@ -17,3 +17,19 @@ function showSlides() {
     dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 2000);
 }
+
+$(document).ready(function () {
+    $(".accordion").click(function () {
+        var $this = $(this);
+        console.log($this);
+        var $answer = $this.parent().siblings('.testimonial__faq__content__box__answer');
+
+        $answer.toggle();
+
+        if ($answer.is(':visible')) {
+            $this.addClass('accordion-open').removeClass('accordion-close');
+        } else {
+            $this.addClass('accordion-close').removeClass('accordion-open');
+        }
+    });
+});
